5a3c8d4276d5fa076a630c91879c9b19d6b28efd
Diego Raggio <didiraggio@gmail.com>
2018-04-05 11:01:53 -0300

    initial commit

---
+++ alarm_clock.py
@@ -0,0 +1,160 @@
+#!usr/bin/env python  
+#coding=utf-8  
+
+import datetime, re
+import pyaudio
+import wave
+import sys
+import os.path
+import time
+
+
+class AlarmClock(object):
+	"""AlarmClock takes an inputted string to set the the alarm time.
+	and plays the corresponding sequence of wav files (of pre recorded words)
+	 at the specified alarm time"""
+	def __init__(self):
+		super(AlarmClock, self).__init__()
+		self.alarm_time_str = self.get_alarm_time()
+		self.alarm_time_list = self.parse_alarm_time_str()
+		self.wav_list = self.select_wav_files()
+		self.sleep_time = self.determine_sleep_time()
+
+
+	def get_alarm_time(self):
+		""" get_alarm_time propts the user to enter a string,
+		which is then verified to be in the correct format with regexp.
+		"""
+		time_pattern = re.compile(r'([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]')
+
+		alarm_set = raw_input("Set alarm time (hh:mm): ")
+
+		while not time_pattern.match(alarm_set):
+			print('Invalid input. Must be expressed in "hh:mm"')
+			alarm_set = raw_input("Set alarm time (hh:mm): ")
+
+		return alarm_set
+
+	def parse_alarm_time_str(self):
+		''' parse_alarm_time_str creates a list of [Hour, First Minute Digit, Second Minute Digit] 
+				'''
+		alarm_time_list = [ int(x) for x in self.alarm_time_str.split(":") ]
+		try:
+			alarm_time_list = [alarm_time_list[0],int(str(alarm_time_list[1])[0]), int(str(alarm_time_list[1])[1])]
+		except:
+			alarm_minutes = int(str(alarm_time_list[1])[0])
+			if alarm_minutes == 0:
+				alarm_time_list = [alarm_time_list[0], alarm_minutes, 0]
+			else:
+				alarm_time_list = [alarm_time_list[0], 0, alarm_minutes]
+		return alarm_time_list
+
+	def select_wav_files(self):
+		''' select_wav_files reads the alart_time_list and determines what wav files to select.
+				if the inputted time is AM or PM, if it adds the "oh" etc. '''
+		# Initiate wav_list
+		wav_list = []
+		alarm_time_hour = self.alarm_time_list[0]
+		alarm_time_minute1 = self.alarm_time_list[1]
+		alarm_time_minute2 = self.alarm_time_list[2]
+
+		# Determine if AM or PM
+		if alarm_time_hour < 12:
+			wav_list.insert(0, "./irishclock/am.wav")
+		else:
+			wav_list.insert(0, "./irishclock/pm.wav")
+			# Adjust PM hour
+			alarm_time_hour = alarm_time_hour - 12
+
+		# Add hour to wav_list
+		if alarm_time_hour == 0:
+			wav_list.insert(0, "./irishclock/12.wav")
+		else:
+			wav_list.insert(0, "./irishclock/{}.wav".format(alarm_time_hour))
+
+		# Add minutes to wav_list
+		# No minutes
+		if alarm_time_minute1 == 0 and alarm_time_minute2 == 0:
+			pass
+		# Single digit minutes
+		elif alarm_time_minute1 == 0 and alarm_time_minute2 != 0:
+			wav_list.insert(1, "./irishclock/o.wav")
+			wav_list.insert(2, "./irishclock/{}.wav".format(alarm_time_minute2))
+		# Minutes between 10 and 19
+		elif alarm_time_minute1 == 1:
+			wav_list.insert(1, "./irishclock/{0}{1}.wav".format(alarm_time_minute1, alarm_time_minute2))
+		# Minutes over 19
+		else:
+			wav_list.insert(1, "./irishclock/{}0.wav".format(alarm_time_minute1))
+			wav_list.insert(2, "./irishclock/{}.wav".format(alarm_time_minute2))
+
+		wav_list.insert(0, "./irishclock/its.wav")
+
+		return wav_list
+
+	def determine_sleep_time(self):
+		'''determine_sleep_time obtains the amount of seconds from the current time to the inputted alarm time'''
+		FMT = '%H:%M'
+
+		alarm_time = datetime.datetime.strptime(self.alarm_time_str, FMT)
+		time_now = datetime.datetime.now()
+
+		sleep_time = (alarm_time - time_now.replace(year=1900, month=01, day=01)).total_seconds()
+
+		if sleep_time < 0:
+			sleep_time += 86400
+		
+		return sleep_time
+
+	CHUNK_SIZE = 1024
+
+	def play_wav(self, wav_filename, chunk_size=CHUNK_SIZE):
+		'''
+		Play (on the attached system sound device) the WAV file
+		named wav_filename.
+		'''
+		try:
+			
+			print( 'Trying to play file ' + wav_filename)
+			wf = wave.open(wav_filename, 'rb')
+		except IOError as ioe:
+			sys.stderr.write('IOError on file ' + wav_filename + '\n' + \
+			str(ioe) + '. Skipping.\n')
+			return
+		except EOFError as eofe:
+			sys.stderr.write('EOFError on file ' + wav_filename + '\n' + \
+			str(eofe) + '. Skipping.\n')
+			return
+
+		# Instantiate PyAudio.
+		p = pyaudio.PyAudio()
+
+		# Open stream.
+		stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+			channels=wf.getnchannels(),
+			rate=wf.getframerate(),
+			output=True,
+			frames_per_buffer = wf.getsampwidth())
+			# output=True)
+
+		data = wf.readframes(chunk_size)
+		while len(data) > 0:
+			stream.write(data)
+			data = wf.readframes(chunk_size)
+
+		# Stop stream.
+		stream.stop_stream()
+		stream.close()
+
+		# Close PyAudio.
+		p.terminate()
+
+	def run(self):
+		# time.sleep(sleep_time)
+		print(self.sleep_time)
+		print(self.wav_list)
+		for wav_filename in self.wav_list:
+			self.play_wav(wav_filename)
+			
+if __name__ == '__main__':
+	AlarmClock().run()
---
+++ test_alarm_clock.py
@@ -0,0 +1,134 @@
+import unittest
+import sys
+from alarm_clock import AlarmClock
+import StringIO
+import mock
+import __builtin__
+from freezegun import freeze_time
+
+
+
+@freeze_time("2015-10-21 16:29:00")
+class TestAlarmClock(unittest.TestCase):
+	#CREATE METHODS TO INITIALIZE TESTING EXAMPLES, BEFORE EACH TEST CASE
+
+	# @mock.patch.object(__builtin__, 'raw_input')
+	# def setUp(self, mock_raw_input):
+		# mock_raw_input.return_value = "00:00"
+	
+	def setUp(self):
+		self.input1 = "00:00"
+		self.input2 = "01:30"
+		self.input3 = "12:05"
+		self.input4 = "14:01"
+		self.input5 = "21:00"
+		self.inputs = ["00:00","01:30","12:05", "14:01", "21:00"]
+		__builtin__.raw_input = mock.Mock(side_effect=self.inputs)
+		self.ac1 = AlarmClock()
+		self.ac2 = AlarmClock()
+		self.ac3 = AlarmClock()
+		self.ac4 = AlarmClock()
+		self.ac5 = AlarmClock()
+		
+
+	def test_initial_alarm_clock(self):
+		
+		ac = self.ac1
+		self.assertIsInstance(ac, AlarmClock)
+		self.assertIsInstance(ac.alarm_time_str, str)
+		self.assertIsInstance(ac.alarm_time_list, list)
+		# print(ac.alarm_time_str)
+		# ac = AlarmClock()
+		# print(ac.alarm_time_str)
+		# ac = AlarmClock()
+		# print(ac.alarm_time_str)
+
+	def test_get_alarm_time(self):
+		# test string format:
+		self.assertRegexpMatches(self.ac1.alarm_time_str, '([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]')
+
+		#test all inputs received
+		self.assertEqual(self.ac1.alarm_time_str, "00:00")
+		self.assertEqual(self.ac2.alarm_time_str,"01:30")
+		self.assertEqual(self.ac3.alarm_time_str,"12:05")
+		self.assertEqual(self.ac4.alarm_time_str,"14:01")
+		self.assertEqual(self.ac5.alarm_time_str,"21:00")
+
+	def test_parse_alarm_time_str(self):
+		# Case 00:00
+		self.assertEqual(self.ac1.alarm_time_list, [0,0,0])
+		# Case 01:30
+		self.assertEqual(self.ac2.alarm_time_list, [1,3,0])
+		# Case 12:05
+		self.assertEqual(self.ac3.alarm_time_list, [12,0,5])
+		# Case 14:01
+		self.assertEqual(self.ac4.alarm_time_list, [14,0,1])
+		# Case 21:00
+		self.assertEqual(self.ac5.alarm_time_list, [21,0,0])
+
+	def test_select_wav_files(self):
+		# Case 00:00
+		self.assertEqual(self.ac1.wav_list, ['./irishclock/its.wav', './irishclock/12.wav', './irishclock/am.wav'])
+		# Case 01:30
+		self.assertEqual(self.ac2.wav_list, ['./irishclock/its.wav', './irishclock/1.wav', './irishclock/30.wav', './irishclock/0.wav', './irishclock/am.wav'])
+		# Case 12:05
+		self.assertEqual(self.ac3.wav_list, ['./irishclock/its.wav', './irishclock/12.wav', './irishclock/o.wav', './irishclock/5.wav', './irishclock/pm.wav'])
+		# Case 14:01
+		self.assertEqual(self.ac4.wav_list, ['./irishclock/its.wav', './irishclock/2.wav', './irishclock/o.wav', './irishclock/1.wav', './irishclock/pm.wav'])
+		# Case 21:00
+		self.assertEqual(self.ac5.wav_list, ['./irishclock/its.wav', './irishclock/9.wav', './irishclock/pm.wav'])
+
+	def test_determine_sleep_time(self):
+		# Case 00:00
+		self.assertEqual(self.ac1.sleep_time, 27060)
+		# Case 01:30
+		self.assertEqual(self.ac2.sleep_time, 32460)
+		# Case 12:05
+		self.assertEqual(self.ac3.sleep_time, 70560)
+		# Case 14:01
+		self.assertEqual(self.ac4.sleep_time, 77520)
+		# Case 21:00
+		self.assertEqual(self.ac5.sleep_time, 16260)
+
+	def test_play_wav(self):
+		# with self.assertRaises(IOError):
+			# self.ac1.play_wav('non_existant_file.wav')
+		# self.ac1.play_wav('./irishclock/pm.wav')
+		# self.assertRaises(IOError,self.ac1.play_wav(),self.ac1, 'non_existant_file.wav')
+		# self.assertEqual(self.ac1.play_wav('non_existant_file.wav'),"blahblah")
+
+		# Test non existing file:
+		capturedOutputError = StringIO.StringIO()
+		capturedOutputPrint = StringIO.StringIO()
+		
+		sys.stderr = capturedOutputError
+		sys.stdout = capturedOutputPrint
+		
+		self.ac1.play_wav('non_existant_file.wav')
+		
+		sys.stderr = sys.__stderr__
+		sys.stdout = sys.__stdout__
+
+		self.assertEqual(capturedOutputError.getvalue(), "IOError on file non_existant_file.wav\n[Errno 2] No such file or directory: 'non_existant_file.wav'. Skipping.\n")
+		self.assertEqual(capturedOutputPrint.getvalue(), "Trying to play file non_existant_file.wav\n")
+
+
+		# Test existing file:
+		capturedOutputError = StringIO.StringIO()
+		capturedOutputPrint = StringIO.StringIO()
+		
+		sys.stderr = capturedOutputError
+		sys.stdout = capturedOutputPrint
+		
+		self.ac1.play_wav('./irishclock/pm.wav')
+		
+		sys.stderr = sys.__stderr__
+		sys.stdout = sys.__stdout__
+
+		self.assertEqual(capturedOutputError.getvalue(), "")
+		self.assertEqual(capturedOutputPrint.getvalue(), "Trying to play file ./irishclock/pm.wav\n")
+
+
+
+if __name__ == '__main__':
+	unittest.main()
